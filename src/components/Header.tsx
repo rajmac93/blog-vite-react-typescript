7ba03b4cb27cde0c541beabe69bbe1ebf1b34123
Maciej Rajtar <rajmac93@gmail.com>
2023-06-06 22:43:01 +0200

    creating logo on sticky bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,9 @@
 import classes from "../styles/Header.module.css";
-import logo from "../assets/icons/logo.svg";
 
 const Header = () => {
   return (
-    <header className={classes.header}>
-      <nav>
-        <ul>
-          <li>
-            <img className={classes.logo} src={logo} alt={logo}></img>
-          </li>
-        </ul>
-      </nav>
+    <header>
+      <h1>DUPA</h1>
     </header>
   );
 };
