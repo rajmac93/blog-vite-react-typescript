41c250d40be0e8abe1f44c17eaba2706e469c110
Maciej Rajtar <rajmac93@gmail.com>
2023-06-14 20:56:56 +0200

    added infinite scrolling, deleting the searching (not working toghether)

--- src/components/data/Posts.tsx
+++ src/components/data/Posts.tsx
@@ -1,32 +1,66 @@
 import { useState } from "react";
+
+import InfiniteScroll from "react-infinite-scroll-component";
+
 import Post from "../Post/Post";
+import LoaderSpinner from "../UI/Loader/LoaderSpinner";
+
 import classes from "./Posts.module.css";
 import Data from "../data/post-data.json";
 
 const Posts = () => {
-  const [query, setQuery] = useState("");
-  const data = Data["data"];
+  // const [query, setQuery] = useState("");
+  const [dataSource, setDataSource] = useState(
+    Array.from(Data["data"].splice(0, 10))
+  );
+
+  const [hasMore, setHasMore] = useState(true);
+  const fetchMoreData = () => {
+    if (dataSource.length) {
+      //SIMLUATION OF API CALL
+      setTimeout(() => {
+        setDataSource(
+          dataSource.concat(
+            Array.from(Data["data"].splice(0, 10))
+          )
+        );
+      }, Math.floor(Math.random() * 1000));
+    } else {
+      setHasMore(false);
+    }
+  };
 
   return (
     <>
-      <input
+      {/* <input
         className={classes["input--title_finder"]}
         placeholder="WPISZ TYTUŁ ARTYKUŁU"
         onChange={(event) => setQuery(event.target.value)}
-      />
-      <div className={classes["posts-container"]}>
-        {data
-          .filter((post) => {
-            if (query === "") {
-              return post;
-            } else if (post.title.toLowerCase().includes(query.toLowerCase())) {
-              return post;
-            }
-          })
+      /> */}
+
+      <InfiniteScroll
+        className={classes["posts-container"]}
+        dataLength={Data["data"].length}
+        next={fetchMoreData}
+        hasMore={hasMore}
+        loader={
+          <LoaderSpinner className={classes.loader} />
+        }
+        endMessage={<p>No more data!</p>}
+      >
+        {dataSource
+          // .filter((post) => {
+          //   if (query === "") {
+          //     return post;
+          //   } else if (post.title.toLowerCase().includes(query.toLowerCase())) {
+          //     return post;
+          //   }
+          // })
+          //? filtering data (commented brcouse of trying with infinite scrolling)
           .map((post) => (
             <Post key={post.id} post={post} />
           ))}
-      </div>
+      </InfiniteScroll>
     </>
   );
 };
